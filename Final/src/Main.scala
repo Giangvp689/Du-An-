28ca5cf88ca7930c470f9c5c8ad3a854c239f9f1
Giang689-vp <giangvp689@gmail.com>
2026-05-18 10:16:31 +0700

    fff

--- Final/src/Main.scala
+++ Final/src/Main.scala
@@ -11,9 +11,7 @@ object Main {
 
     import spark.implicits._
 
-  AAAAA
-
-    // 3.1 Đọc dữ liệu nha  OK Nhe WWWW
+   
     val dfRaw = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
